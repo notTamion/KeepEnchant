3067092f19136505929b5bc016e7720b921c6081
Tamion <simon.dorner.obf@gmail.com>
2023-06-23 19:04:30 +0200

    Initial Commit

---
+++ src/main/java/de/tamion/KeepEnchant.java
@@ -0,0 +1,22 @@
+package de.tamion;
+
+import de.tamion.listeners.InventoryListeners;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KeepEnchant extends JavaPlugin {
+
+    private static KeepEnchant plugin;
+    @Override
+    public void onEnable() {
+        PluginManager pluginManager = Bukkit.getPluginManager();
+        plugin = this;
+
+        pluginManager.registerEvents(new InventoryListeners(), this);
+    }
+
+    public static KeepEnchant getPlugin() {
+        return plugin;
+    }
+}
