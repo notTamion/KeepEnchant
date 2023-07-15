963ff9647444c113f096347deec025b3220bf24e
Tamion <simon.dorner.obf@gmail.com>
2023-07-15 07:29:56 +0200

    Fixed some things

--- src/main/java/de/tamion/listeners/InventoryListeners.java
+++ src/main/java/de/tamion/listeners/InventoryListeners.java
@@ -1,35 +1,50 @@
 package de.tamion.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
-
-import java.util.Objects;
 
 public class InventoryListeners implements Listener {
     @EventHandler
     public void onInventoryClick(InventoryClickEvent e) {
         try {
-            if (!e.getInventory().getType().equals(InventoryType.GRINDSTONE) || e.getCurrentItem() == null || e.getCurrentItem().getType().isAir() || !Objects.equals(e.getCurrentItem(), e.getView().getItem(2))) {
+            if (!e.getInventory().getType().equals(InventoryType.GRINDSTONE) || e.getCurrentItem() == null || e.getCurrentItem().getType().isAir() || e.getSlot() != 2) {
                 return;
             }
             Player p = (Player) e.getWhoClicked();
             ItemStack book = new ItemStack(Material.ENCHANTED_BOOK);
             EnchantmentStorageMeta bookmeta = (EnchantmentStorageMeta) book.getItemMeta();
-            int originalitemslot = 0;
-            if (e.getView().getItem(0).getType().isAir()) {
-                originalitemslot = 1;
+            if (!e.getView().getItem(0).getType().isAir()) {
+                e.getView().getItem(0).getEnchantments().forEach((enchantment, integer) -> {
+                    bookmeta.addStoredEnchant(enchantment, integer, true);
+                });
             }
-            e.getView().getItem(originalitemslot).getEnchantments().forEach((enchantment, integer) -> {
-                bookmeta.addStoredEnchant(enchantment, integer, true);
-            });
+            if (!e.getView().getItem(1).getType().isAir()) {
+                e.getView().getItem(1).getEnchantments().forEach((enchantment, integer) -> {
+                    bookmeta.addStoredEnchant(enchantment, integer, true);
+                });
+            }
+            if(bookmeta.getStoredEnchants().isEmpty()) {
+                return;
+            }
             book.setItemMeta(bookmeta);
             if (p.getInventory().firstEmpty() == -1) {
+                if(!e.isShiftClick()) {
+                    p.getWorld().dropItem(p.getLocation(), book);
+                }
+                return;
+            }
+            Inventory playinv = Bukkit.createInventory(null, 9*3);
+            playinv.setContents(p.getInventory().getContents());
+            playinv.addItem(book);
+            if(playinv.firstEmpty() == -1) {
                 p.getWorld().dropItem(p.getLocation(), book);
                 return;
             }
